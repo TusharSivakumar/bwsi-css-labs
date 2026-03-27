468ef0cfff97fb66fb98cc15f7eda9c98e2be588
TusharSivakumar <2shars0306@gmail.com>
2026-03-27 19:01:58 -0400

    added description to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Tushar Sivakumar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello, my name is Tushar Sivakumar, and I am from  Massachusetts. I am a big music fan, and listen to various genres, and I love trying new food, especially spicy food. Finally, I like to code and make new projects.")
 
 if __name__ == "__main__":
     main()
